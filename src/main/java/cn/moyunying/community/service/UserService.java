2f575cdfd012cf34b0fc47af687d4b6be0cbc257
moyunying <1743977594@qq.com>
2021-08-31 09:47:36 +0800

    开发社区首页

---
+++ src/main/java/cn/moyunying/community/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.moyunying.community.service;
+
+import cn.moyunying.community.dao.UserMapper;
+import cn.moyunying.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+}
