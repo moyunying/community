c95c45e85cb016ea8fdebd4b38b53d227b3033fa
moyunying <1743977594@qq.com>
2021-10-16 15:36:07 +0800

    网站数据统计

--- src/main/java/cn/moyunying/community/config/WebMvcConfig.java
+++ src/main/java/cn/moyunying/community/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package cn.moyunying.community.config;
 
+import cn.moyunying.community.controller.interceptor.DataInterceptor;
 import cn.moyunying.community.controller.interceptor.LoginRequiredInterceptor;
 import cn.moyunying.community.controller.interceptor.LoginTicketInterceptor;
 import cn.moyunying.community.controller.interceptor.MessageInterceptor;
@@ -20,6 +21,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private MessageInterceptor messageInterceptor;
 
+    @Autowired
+    private DataInterceptor dataInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
@@ -29,6 +33,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(dataInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
