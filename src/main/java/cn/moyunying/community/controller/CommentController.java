604a08f5904efdbcb2e73456c3ad8172a67f9a3b
moyunying <1743977594@qq.com>
2021-10-19 18:00:41 +0800

    热帖排行

--- src/main/java/cn/moyunying/community/controller/CommentController.java
+++ src/main/java/cn/moyunying/community/controller/CommentController.java
@@ -8,7 +8,9 @@ import cn.moyunying.community.service.CommentService;
 import cn.moyunying.community.service.DiscussPostService;
 import cn.moyunying.community.util.CommunityConstant;
 import cn.moyunying.community.util.HostHolder;
+import cn.moyunying.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,9 @@ public class CommentController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping(path = "/add/{discussPostId}", method = RequestMethod.POST)
     public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
@@ -63,6 +68,10 @@ public class CommentController implements CommunityConstant {
                     .setEntityType(ENTITY_TYPE_POST)
                     .setEntityId(discussPostId);
             eventProducer.fireEvent(event);
+
+            // 计算帖子分数
+            String redisKey = RedisKeyUtil.getPostScoreKey();
+            redisTemplate.opsForSet().add(redisKey, discussPostId);
         }
 
         return "redirect:/discuss/detail/" + discussPostId;
--- src/main/java/cn/moyunying/community/controller/LikeController.java
+++ src/main/java/cn/moyunying/community/controller/LikeController.java
@@ -8,7 +8,9 @@ import cn.moyunying.community.entity.User;
 import cn.moyunying.community.util.CommunityConstant;
 import cn.moyunying.community.util.CommunityUtil;
 import cn.moyunying.community.util.HostHolder;
+import cn.moyunying.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +30,9 @@ public class LikeController implements CommunityConstant {
 
     @Autowired
     private EventProducer eventProducer;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @LoginRequired
     @RequestMapping(path = "/like", method = RequestMethod.POST)
@@ -56,6 +61,12 @@ public class LikeController implements CommunityConstant {
                     .setEntityUserId(entityUserId)
                     .setData("postId", postId);
             eventProducer.fireEvent(event);
+        }
+
+        if (entityType == ENTITY_TYPE_POST) {
+            // 计算帖子分数
+            String redisKey = RedisKeyUtil.getPostScoreKey();
+            redisTemplate.opsForSet().add(redisKey, postId);
         }
 
         return CommunityUtil.getJSONString(0, null, map);
